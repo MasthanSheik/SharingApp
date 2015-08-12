4c662632a33d6a2451fc5a381828439017cba7d3
osmosys <itsupport@osmosys.asia>
2015-08-12 16:05:25 +0530

    committed shareapp

---
+++ www/js/app.js
@@ -0,0 +1,34 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var ionicApp= angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+ionicApp.controller("ExampleController", function($scope, $cordovaSocialSharing) {
+ 
+    $scope.shareAnywhere = function() {
+        $cordovaSocialSharing.share("Osmosys", "Osmosys webstation", "img/osm.png", "webstation.osmosystems.com");
+    }
+ 
+    $scope.shareViaTwitter = function(message, image, link) {
+        $cordovaSocialSharing.canShareVia("twitter", message, image, link).then(function(result) {
+            $cordovaSocialSharing.shareViaTwitter(message, image, link);
+        }, function(error) {
+            alert("Cannot share on Twitter");
+        });
+    }
+ 
+});
